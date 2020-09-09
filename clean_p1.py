ca0d61805cbcdfa84a8383a1be366fb4b8c87b52
Will Clifford <wtc9854@rit.edu>
2020-09-09 18:45:14 -0400

    folder structure, beginning to clean

--- clean_p1.py
+++ clean_p1.py
@@ -1,1 +1,17 @@
 import pandas as pd
+import os
+
+def filter(df):
+    
+    return df
+
+
+def main():
+    rcdb = pd.DataFrame()
+    for fname in os.listdir('rcdb'):
+        rcdb = rcdb.append(pd.read_csv('rcdb/' + fname), ignore_index=True)
+    print(rcdb)
+
+
+if __name__ == '__main__':
+    main()
